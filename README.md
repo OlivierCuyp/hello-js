94cbde7d9e415a51bfbe949b5ac0906056077a25
Olivier Cuypers <oliviercuyp@gmail.com>
2018-09-21 10:51:49 +0200

    add debug routes
    - /ip
    - /headers
    - /cookies

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # hello-js ![Docker Build](https://img.shields.io/docker/build/oliviercuyp/hello-js.svg)
 
 A simple expressjs server to ops tests.
+
+## Routes
+
+- **GET** `/` respond: `hello <name>` (name is env.NAME or default to "JS")
+- **GET** `/ip` respond: `{ ip: x.x.x.x }` (expose client ip)
+- **GET** `/headers` respond: `{ headers:  { ... } }` (expose req.headers)
+- **GET** `/cookies` respond: `{ cookies:  { ... } }` (expose req.cookies)
+
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
 const app = express();
 const port = process.env.APP_PORT || 3000;
 const name = process.env.NAME || 'JS';
 
+app.use(cookieParser());
+
 app.get('/', (req, res) => res.send(`Hello ${name}!`));
+app.get('/ip', (req, res) => {
+  const ip = req.headers['x-forwarded-for'] ||
+    req.connection.remoteAddress ||
+    req.socket.remoteAddress ||
+    req.connection.socket.remoteAddress;
+  res.json({ ip });
+});
+app.get('/headers', (req, res) => res.json(req.headers));
+app.get('/cookies', (req, res) => res.json({ cookies: req.cookies }));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
